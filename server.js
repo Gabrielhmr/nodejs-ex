891518f23722afabcb87b174981957f4e1986bc5
Gabriel Rodrigues <gabriel.rodrigues.uece@gmail.com>
2017-12-18 01:55:17 -0200

    post body

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ var express = require('express'),
 var cors = require('cors');
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodie
+
 
 
 const paypal = require('paypal-rest-sdk');
